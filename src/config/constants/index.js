71e483a54f853a0a1e1cacae1e790241de97390a
Sooraj Kumar <soorajharani.sh@gmail.com>
2022-10-10 17:50:57 +0500

    fixes

--- src/config/constants/index.js
+++ src/config/constants/index.js
@@ -1,8 +1,8 @@
 const Images = {};
 const Endpoints = {};
 
-// const baseURL = "https://admin.mjafferjees.com"; //UAT
-const baseURL = "http://3.135.102.9:9117"; //QA
+const baseURL = "https://admin.mjafferjees.com"; //UAT
+// const baseURL = "http://3.135.102.9:9117"; //QA
 
 const currencyAPIKey = "9imYcW3HYByekTlES1Ut4jU2pwKK6UOR";
 
